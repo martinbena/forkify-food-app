3d061dbfc78936e7b4da024f488252cc14f5b4b7
martinbena <bena.m@seznam.cz>
2023-01-12 14:38:54 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "8e6a8095-9c22-42eb-9bf6-4163d7a1049a";
+export const MODAL_CLOSE_SEC = 2.5;
